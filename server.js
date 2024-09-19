0efca7fbe88a1c2852e0cd46410c4ff58c1e1e23
christinlazar <christinlazar19@gmail.com>
2024-09-19 10:42:38 +0530

    first commit from server side

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const upload = require('./utils/multer')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { PDFDocument } = require('pdf-lib')
+const path = require('path')
+const fs = require('fs')
+const app = express()
+const PORT = 5000
+app.use(bodyParser.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors({
+    origin:"http://localhost:3000",
+    methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials:true,
+    optionsSuccessStatus:200}))
+
+
+app.post('/upload',upload.single('pdf'),async (req,res)=>{
+    try {
+        if(!req.file){
+            return res.json({success:false})
+        }else{
+            return res.json({success:true})
+        }
+    }catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.post('/extract',upload.single('pdf'),async (req,res)=>{
+   const filePath = path.join(__dirname, req.file.path)
+    const pagesToExtract = JSON.parse(req.body.pages)
+    try {
+        const pdfBytes = fs.readFileSync(filePath)
+        const pdfDoc = await PDFDocument.load(pdfBytes)
+        const newDoc = await PDFDocument.create()
+        for(let page of pagesToExtract){
+            const[copiedPage] = await newDoc.copyPages(pdfDoc,[page - 1])
+            newDoc.addPage(copiedPage)
+        }
+        const newPdfBytes = await newDoc.save()
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename="extracted.pdf"',
+          });
+      
+          res.send(Buffer.from(newPdfBytes));
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ success: false, message: error.message });
+    }finally {
+        fs.unlink(filePath, (err) => {
+          if (err) console.error('Error deleting file:', err);
+        });
+      }
+})
+
+app.listen(PORT,()=>{
+    console.log( `server is running on port ${PORT}` )
+})
